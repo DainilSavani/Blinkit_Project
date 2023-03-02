23dc99cfe3211c25a0df1c4b91ce8d9e3241cb31
DainilSavani <savanidainil@gmail.com>
2023-03-02 12:23:20 +0530

    Shifted the billing page to the react

--- blinkit/src/pages/BillingPage.js
+++ blinkit/src/pages/BillingPage.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+
+//constants and helper functions fetching
 import VEGETABLE_ITEMS from '../data/vegetableData.json';
+import {CART_STATUS} from '../data/constData';
 import { searchVegetable, addItemsToCartHandler, decreaseItemsFromCartHandler, 
          removeItemFromCartHandler, clearCartHandler } from '../helperFunction/stateChangeFunction';
-import {CART_STATUS} from '../data/constData';
+
+//componenets fetching
 import Navbar from './components/billingPage/organisms/header/Navbar';
 import MainBillingSection from './components/billingPage/organisms/main/MainBillingSection';
 import SubHeaderCategories from './components/billingPage/molecules/header/SubHeaderCategories';
@@ -17,6 +21,7 @@ class BillingPage extends Component {
             cartStatus: CART_STATUS,
             searchBarValue: "",
         }
+        //binding helper functions
         this.searchVegetable = searchVegetable.bind(this);
         this.addItemsToCartHandler = addItemsToCartHandler.bind(this);
         this.decreaseItemsFromCartHandler = decreaseItemsFromCartHandler.bind(this);
@@ -30,9 +35,7 @@ class BillingPage extends Component {
                 <Navbar searchVegetable={this.searchVegetable} cartStatus={this.state.cartStatus} />
                 <SubHeaderCategories />
                 <MainBillingSection
-                    vegetableItems={this.state.vegetableItems}
-                    cartStatus={this.state.cartStatus}
-                    searchBarValue={this.state.searchBarValue}
+                    {...this.state}
                     addItemsToCartHandler={this.addItemsToCartHandler}
                     decreaseItemsFromCartHandler={this.decreaseItemsFromCartHandler}
                     removeItemFromCartHandler={this.removeItemFromCartHandler}
@@ -45,3 +48,4 @@ class BillingPage extends Component {
 }
 
 export default BillingPage
+
