384aacb4ec58304000729c63b9f8021d5c3a7578
DainilSavani <savanidainil@gmail.com>
2023-03-06 15:26:40 +0530

    Made changes according to the comments

---
+++ src/pages/components/homePage/molecules/footer/Foot.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import FootMid from '../atoms/footer/FootMid';
+import FootRight from '../atoms/footer/FootRight';
+import './foot.scss';
+
+class Foot extends PureComponent {
+    render() {
+        return (
+            <section className='foot'>
+                <div className="footLeft">
+                    © Blink Commerce Private Limited (formerly known as Grofers<br /> India Private Limited), 2016-2023
+                </div>
+                <FootMid />
+                <FootRight />
+            </section>
+        )
+    }
+}
+
+export default Foot
