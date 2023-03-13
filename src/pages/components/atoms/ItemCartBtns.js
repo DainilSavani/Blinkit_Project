969ea7ac503e16fac2a8d3fe885bb9ffa4918154
DainilSavani <savanidainil@gmail.com>
2023-03-13 15:49:25 +0530

    made some changes

--- src/pages/components/atoms/ItemCartBtns.js
+++ src/pages/components/atoms/ItemCartBtns.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 
 class ItemCartBtns extends Component {
     render() {
-        const {addToCartHandler, removeFromCartHandler, itemIndex, vegetableItem} = this.props;
+        const {incrementItemInCartHandler, decrementItemInCartHandler, itemIndex, vegetableItem} = this.props;
         return (
             <div className="billItemBtn">
-                <button className='decreaseItem' onClick={() => removeFromCartHandler(itemIndex)}>-</button>
+                <button className='decreaseItem' onClick={() => decrementItemInCartHandler(itemIndex)}>-</button>
                 <div className='itemValue'>{vegetableItem.count}</div>
-                <button className='increaseItem' onClick={() => addToCartHandler(itemIndex)}>+</button>
+                <button className='increaseItem' onClick={() => incrementItemInCartHandler(itemIndex)}>+</button>
             </div>
         )
     }
