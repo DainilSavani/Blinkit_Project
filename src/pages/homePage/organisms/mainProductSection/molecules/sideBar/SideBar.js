616634d115812fa74ef54c90a459e00ea5834ca6
DainilSavani <savanidainil@gmail.com>
2023-03-29 12:12:40 +0530

    made final change

--- src/pages/homePage/organisms/mainProductSection/molecules/sideBar/SideBar.js
+++ src/pages/homePage/organisms/mainProductSection/molecules/sideBar/SideBar.js
@@ -1,46 +1,29 @@
 import React, { Component } from 'react';
 
+// constants
+import SIDEBAR_CATEGORIES from './constants/sideBarConstants';
+
 // components
 import SideBarItem from './atoms';
 
 // style
 import './sideBar.scss';
 
-const SIDEBAR_CATEGORIES = [
-    {
-        name: 'All',
-        src: 'https://cdn.grofers.com/app/images/category/cms_images/icon/1489_1643613620694.png',
-        alt: 'all'
-    }, {
-        name: 'Fresh Vegetables',
-        src: 'https://cdn.grofers.com/66acfb51-c5fe-4718-a200-61efaf773556.png',
-        alt: 'fresh vegetables'
-    }, {
-        name: 'Exotics',
-        src: 'https://cdn.grofers.com/app/images/category/cms_images/icon/278_1678705041060.png',
-        alt: 'exotics'
-    }, {
-        name: 'Seasonal',
-        src: 'https://cdn.grofers.com/app/images/category/cms_images/icon/482_1678341687886.png',
-        alt: 'seasonal'
-    }, {
-        name: 'Organic & Hydroponic',
-        src: 'https://cdn.grofers.com/app/images/category/cms_images/icon/39_1677581549982.png',
-        alt: 'organic'
-    }, {
-        name: 'Freshly Cut & Sprouts',
-        src: 'https://cdn.grofers.com/app/images/category/cms_images/icon/395_1668582176947.png',
-        alt: 'freshly cut'
-    }
-]
+const renderSideBarCategories = () =>
+    SIDEBAR_CATEGORIES.map((item, index) =>
+        <SideBarItem key={index} sideItem={item} index={index} />
+    )
 
 class SideBar extends Component {
+    constructor(props) {
+        super(props)
+        this.renderSideBarCategories = renderSideBarCategories();
+    }
 
-    loadSideBarItems = () => SIDEBAR_CATEGORIES.map((item, index) => <SideBarItem key={index} sideItem={item} index={index}></SideBarItem>)
     render() {
         return (
             <aside className='cat'>
-                {this.loadSideBarItems()}
+                {this.renderSideBarCategories}
             </aside>
         )
     }
